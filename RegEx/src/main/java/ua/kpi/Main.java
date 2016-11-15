6555e51a0202555ec80a6c33e0deba78d4770f10
jekafortuna <evgeniy.fortuna@gmail.com>
2016-11-15 12:23:58 +0200

    Notebook

--- RegEx/src/main/java/ua/kpi/Main.java
+++ RegEx/src/main/java/ua/kpi/Main.java
@@ -9,5 +9,12 @@ package ua.kpi;
 public class Main {
     public static void main(String[] args) {
 
+        // Initialization
+        Notebook notebook = new Notebook();
+        View view = new View();
+        Controller controller = new Controller(notebook, view);
+
+        // Run
+        controller.processUser();
     }
 }
